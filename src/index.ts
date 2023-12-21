0f4cfb7a5efe6f041ef39a545b42e6ae8528d39b
Hong Van <hongvanpc10@gmail.com>
2023-12-21 13:57:03 +0700

    Initialize project with expressjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+import 'dotenv/config'
+import express from 'express'
+import { PORT } from './configs/env'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const port = PORT || 8000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
